5fbbd7a45c328b378abc39f7dadcad7bcd89c3ca
Arjun Ankathatti Chandrashekara <acarjungowda@gmail.com>
2018-12-14 13:34:09 -0600

    made few styling changes

--- src/js/chatbot.js
+++ src/js/chatbot.js
@@ -68,7 +68,7 @@ var loadChatWindow = function(){
     // api.ai Credentials
     // please change the accessToken to configure this to work with yoru Dialogflow agent
     var baseUrl = "https://api.api.ai/v1/query?v=20160910&";
-    var accessToken = "8cbe1c4eb8484f738b3e12cb4090683b";
+    var accessToken = "6e5c2c2195084691937add3ae219e397";
 
     if(typeof($.fn.popover) != 'undefined') {
         console.log("bootstrap is already loaded");
@@ -79,13 +79,13 @@ var loadChatWindow = function(){
     }
     // Copy the compiled minified CSS from assets/css/chatbot.css 
     // and set chatCSSStyle variable with the copied content as shown below
-    var chatCSSStyle = "<style type=\"text/css\">.chatbox{position:fixed;bottom:0;right:0;width:350px;height:100vh;background-color:#fff;font-family:'Lato',sans-serif;-webkit-transition:all 600ms cubic-bezier(.19,1,.22,1);transition:all 600ms cubic-bezier(.19,1,.22,1);display:-webkit-flex;display:flex;-webkit-flex-direction:column;flex-direction:column;z-index:1000}.chatbox--tray{bottom:calc(50px - 100vh)}.chatbox--closed{bottom:-100vh}.chatbox .form-control:focus{border-color:#1f2836}.chatbox__body,.chatbox__title{border-bottom:none}.chatbox__title{color:#fdc42e;min-height:50px;padding-right:10px;background-color:#1f2836;border-top-left-radius:4px;border-top-right-radius:4px;cursor:pointer;display:-webkit-flex;display:flex;-webkit-align-items:center;align-items:center}.chatbox__title h5{height:50px;margin:0 0 0 15px;line-height:50px;position:relative;padding-left:20px;-webkit-flex-grow:1;flex-grow:1}.chatbox__title h5 a{color:#fdc42e;max-width:195px;display:inline-block;text-decoration:none;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}.chatbox__title h5:before{content:'';display:block;position:absolute;top:50%;left:0;width:12px;height:12px;background:#4caf50;border-radius:6px;-webkit-transform:translateY(-50%);transform:translateY(-50%)}.chatbox__title__close,.chatbox__title__tray{width:24px;height:24px;outline:0;border:0;background-color:transparent;opacity:.5;cursor:pointer;-webkit-transition:opacity 200ms;transition:opacity 200ms}.chatbox__title__close:hover,.chatbox__title__tray:hover{opacity:1}.chatbox__title__tray span{width:12px;height:12px;display:inline-block;border-bottom:2px solid #fff}.chatbox__title__close svg{vertical-align:middle;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.2px}.chatbox__body,.chatbox__credentials{padding:15px;border-top:0;background-color:#fff;border-left:1px solid #ddd;border-right:1px solid #ddd;-webkit-flex-grow:1;flex-grow:1}.chatbox__credentials{display:none}.chatbox__credentials .form-control{-webkit-box-shadow:none;box-shadow:none}.chatbox__body{overflow-y:auto}.chatbox__body__message{position:relative}.chatbox__body__message p{padding:10px;border-radius:4px;font-size:14px;background-color:#fff;-webkit-box-shadow:1px 1px rgba(100,100,100,.1);box-shadow:1px 1px rgba(100,100,100,.1)}.chatbox__body__message img{width:40px;height:40px;border-radius:4px;border:2px solid #fcfcfc;position:absolute;top:5px}.chatbox__body__message--left p{width:85%;float:left;text-align:left;background-color:#e5f1d8}.chatbox__body__message--left img{left:-5px}.chatbox__body__message--right p{width:85%;float:right;text-align:right;background-color:#ddeaf1}.chatbox__body__message--right img{right:-5px}.chatbox__message{padding:15px;min-height:50px;outline:0;resize:none;font-size:12px;border:1px solid #ddd;border-bottom:none;background-color:#fefefe}.chatbox--empty{height:300px}.chatbox--empty.chatbox--tray{bottom:-250px}.chatbox--empty.chatbox--closed{bottom:-300px}.chatbox--empty .chatbox__body,.chatbox--empty .chatbox__message{display:none}.chatbox--empty .chatbox__credentials{display:block}</style>";
-    $('head').append(chatCSSStyle);
+    //var chatCSSStyle = "<style type=\"text/css\">.chatbox{position:fixed;bottom:0;right:0;width:350px;height:70vh;background-color:#fff;font-family:'Lato',sans-serif;-webkit-transition:all 600ms cubic-bezier(.19,1,.22,1);transition:all 600ms cubic-bezier(.19,1,.22,1);display:-webkit-flex;display:flex;-webkit-flex-direction:column;flex-direction:column;z-index:1000}.chatbox--tray{bottom:calc(50px - 100vh)}.chatbox--closed{bottom:-100vh}.chatbox .form-control:focus{border-color:#1f2836}.chatbox__body,.chatbox__title{border-bottom:none}.chatbox__title{color:#fdc42e;min-height:50px;padding-right:10px;background-color:#1f2836;border-top-left-radius:4px;border-top-right-radius:4px;cursor:pointer;display:-webkit-flex;display:flex;-webkit-align-items:center;align-items:center}.chatbox__title h5{height:50px;margin:0 0 0 15px;line-height:50px;position:relative;padding-left:20px;-webkit-flex-grow:1;flex-grow:1}.chatbox__title h5 a{color:#fdc42e;max-width:195px;display:inline-block;text-decoration:none;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}.chatbox__title h5:before{content:'';display:block;position:absolute;top:50%;left:0;width:12px;height:12px;background:#4caf50;border-radius:6px;-webkit-transform:translateY(-50%);transform:translateY(-50%)}.chatbox__title__close,.chatbox__title__tray{width:24px;height:24px;outline:0;border:0;background-color:transparent;opacity:.5;cursor:pointer;-webkit-transition:opacity 200ms;transition:opacity 200ms}.chatbox__title__close:hover,.chatbox__title__tray:hover{opacity:1}.chatbox__title__tray span{width:12px;height:12px;display:inline-block;border-bottom:2px solid #fff}.chatbox__title__close svg{vertical-align:middle;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.2px}.chatbox__body,.chatbox__credentials{padding:15px;border-top:0;background-color:#fff;border-left:1px solid #ddd;border-right:1px solid #ddd;-webkit-flex-grow:1;flex-grow:1}.chatbox__credentials{display:none}.chatbox__credentials .form-control{-webkit-box-shadow:none;box-shadow:none}.chatbox__body{overflow-y:auto}.chatbox__body__message{position:relative}.chatbox__body__message p{padding:10px;border-radius:4px;font-size:14px;background-color:#fff;-webkit-box-shadow:1px 1px rgba(100,100,100,.1);box-shadow:1px 1px rgba(100,100,100,.1)}.chatbox__body__message img{width:40px;height:40px;border-radius:4px;border:2px solid #fcfcfc;position:absolute;top:5px}.chatbox__body__message--left p{width:85%;float:left;text-align:left;background-color:#e5f1d8}.chatbox__body__message--left img{left:-5px}.chatbox__body__message--right p{width:85%;float:right;text-align:right;background-color:#ddeaf1}.chatbox__body__message--right img{right:-5px}.chatbox__message{padding:15px;min-height:50px;outline:0;resize:none;font-size:12px;border:1px solid #ddd;border-bottom:none;background-color:#fefefe}.chatbox--empty{height:300px}.chatbox--empty.chatbox--tray{bottom:-250px}.chatbox--empty.chatbox--closed{bottom:-300px}.chatbox--empty .chatbox__body,.chatbox--empty .chatbox__message{display:none}.chatbox--empty .chatbox__credentials{display:block}</style>";
+    //$('head').append(chatCSSStyle);
 
     // --------- Start Chat Window HTML -------------//
     var chatWindowHTML = "<div class=\"chatbox chatbox--tray chatbox--empty\"> \
     <div class=\"chatbox__title\"> \
-    <h5><a href=\"#\">VIDURA Advisor</a></h5> \
+    <h5><a href=\"#\">Virtual Agent</a></h5> \
     <!-- minimize button --> \
     <button class=\"chatbox__title__tray\"> \
     <span></span> \
@@ -132,17 +132,21 @@ var loadChatWindow = function(){
     // ------------
     $chatboxTitle.on('click', function() {
         $chatbox.toggleClass('chatbox--tray');
-        });
+        if ($chatbox.hasClass('chatbox--closed')) 
+            $chatbox.removeClass('chatbox--closed'), 
+            $chatbox.addClass('chatbox--tray');
+
+    });
 
     // -------------  execute this when close button is clicked   ---------------------//
     $chatboxTitleClose.on('click', function(e) {
-            e.stopPropagation();
-            $chatbox.addClass('chatbox--closed');
+        e.stopPropagation();
+        $chatbox.addClass('chatbox--closed');
     });
 
     // -------------     ---------------------//
     $chatbox.on('transitionend', function() {
-        if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
+        // if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
     });
 
     // ----------- submit button function in the chatbot window -------------//
@@ -151,14 +155,16 @@ var loadChatWindow = function(){
         $chatbox.removeClass('chatbox--empty');
         var userName = $('#inputName').val();
         var userEmail = $('#inputEmail').val();
-        setBotResponse('Hello <b>' + userName + '</b>!, welcome to CyNeuro portal, I am VIDURA your virtual agent.How can I help you?');
+        //setBotResponse('<span class="alert alert-warning">suggestions 01</span>');
+        sendUserText('My name is ' + userName);
+        $("#user_input").focus();
     });
 
 
     // given a string set the usertext in the chat window with appropriate styling
     function setUserText(val) {
-        var userTextBefore = '<div class="chatbox__body__message chatbox__body__message--right"><p>';
-        var userTextAfter = '</p></div>';
+        var userTextBefore = '<p class="userText">';
+        var userTextAfter = '</p>';
         var userTextFinal = userTextBefore + val + userTextAfter;
         $('#chatbox_body_content').append(userTextFinal);
         // set the value of input field to empty string
@@ -178,8 +184,8 @@ var loadChatWindow = function(){
             } else {
                 val = val.replace(new RegExp('\r?\n','g'), '<br />');
             }
-        var botResponseBefore = '<div class="chatbox__body__message chatbox__body__message--left"><p>';
-        var botResponseAfter = '</p></div>';
+        var botResponseBefore = '<p class="botResponse">';
+        var botResponseAfter = '</p>';
         var botResponseFinal = botResponseBefore + val + botResponseAfter;
         $('#chatbox_body_content').append(botResponseFinal);
         // scroll to the bottom of the chatbot body
@@ -284,10 +290,46 @@ var loadChatWindow = function(){
             default:
                 setBotResponse(speech);
                 if(suggestions) { // check if quick replies are there in api.ai
-                    //addSuggestion(suggestions);
+                    addSuggestion(suggestions);
                 }
                 break;
         }
     }
+
+    // Suggestions -----------------------------------------------------------------------------------------
+    function addSuggestion(textToAdd) {
+        setTimeout(function(){
+            var suggestions = textToAdd.replies;
+            var suggLength = textToAdd.replies.length;
+
+            var botResponseBefore = '<div class="suggestions"><div class="sugg-title">Suggestions:</div>';
+            var botResponseAfter = '</div>';
+
+
+            var val = '';
+            
+
+            // Loop through suggestions
+            for(i=0;i<suggLength;i++) {
+                val += '<span class="sugg-options">' + suggestions[i] + '</span>';
+            }
+
+            var botResponseFinal = botResponseBefore + val + botResponseAfter;
+            $('#chatbox_body_content').append(botResponseFinal);
+
+            // scroll to the bottom of the chatbot body
+            $('#chatbox_body_content').scrollTop(1E10);
+
+        }, 1000);
+    }
+
+    // on click of suggestions get value and send to API.AI
+    $(document).on("click", ".suggestions span", function() {
+        var text = this.innerText;
+        setUserText(text);
+        sendUserText(text);
+        $('.suggestions').remove();
+    });
+    // Suggestions end -----------------------------------------------------------------------------------------
 
 }
